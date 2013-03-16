96a182defb52d9341561764faada7c0ab961879d
Johnson Arokiaraj <johnson@activesphere.com>
2013-03-16 13:03:01 +0530

    fix devise name

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,4 +1,4 @@
-class Users::OmniauthCallbacksController < Device::OmniauthCallbacksController
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     binding.pry
     @user = User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user)
